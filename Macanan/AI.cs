761d4478201b69c97b22ec1d77b503e7fc9f5430
Gerry Surya <ger_26_6@hotmail.com>
2018-01-03 23:13:43 +0700

    mungkin ai?

--- Macanan/AI.cs
+++ Macanan/AI.cs
@@ -9,14 +9,57 @@ namespace Macanan
 {
     class AI
     {
-        Point bestMove;
-        public Point getBestMove()
+        //https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-3-tic-tac-toe-ai-finding-optimal-move/
+        public Point getBestMove(char[,] peta, int eaten)
         {
+            Point bestMove = new Point();
             return bestMove;
         }
-        public void calculate(char[,] peta, int level, int eaten)
+        public int evaluate(char[,] peta, int reward = 100)
         {
+            //kiri,kanan,atas,bawah,kiriatas,kiribawah,kananatas,kananbawah
+            string arah;
+            int score = 0;
+            int tempScore;
 
+            Point posMacan = new Point();
+            //CARI POSISI MACAN
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (peta[i,j] == 'M')
+                    {
+                        posMacan.X = j;
+                        posMacan.Y = i;
+                        break;
+                    }
+                }
+            }
+
+            //CARI SEMUA KEMUNGKINAN
+            //KEKANAN
+            tempScore = 1;
+            for (int i = (int)posMacan.X; i < 9; i++)
+            {
+                int jumlahGerak = i - (int)posMacan.X;
+                if (jumlahGerak % 2 == 1)
+                {
+                    if (peta[i, (int)posMacan.Y] == 'Y')
+                    {
+                        tempScore *= 10;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public int minimax(char[,] peta, int depth, bool isMax, int eaten)
+        {
+            int score = evaluate(peta);
+
+            return 0;
         }
     }
 }
